b516980fb97ff3b96e90a9fd3450996d0585aa50
Daniel Sagua Nahuelhual <daniel.sagua@correoaiep.cl>
2025-05-24 03:37:15 -0400

    link

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT} Ingresa en http://localhost:3000/`));
